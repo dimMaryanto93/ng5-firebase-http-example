a232c2af602167a147e622c1fce13c4b424f034d
dimmaryanto93 <engineer.dimmaryanto93@outlook.com>
2017-12-30 17:40:39 +0700

    simple craete an observable my own

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import {Observer} from 'rxjs/Observer';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  ngOnInit(): void {
+    const messages = Observable.create((observer: Observer<string>) => {
+      setTimeout(() => {
+        observer.next('data pertama');
+      }, 2000);
+      setTimeout(() => {
+        observer.next('data kedua');
+      }, 4000);
+      setTimeout(() => {
+        observer.complete();
+      }, 4000);
+    });
+    messages.subscribe(
+      data => console.log(data),
+      error => console.log(error),
+      () => console.log('complate message')
+    );
+  }
 }
